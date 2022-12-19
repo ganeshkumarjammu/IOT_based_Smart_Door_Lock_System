e328953abdd31567fe457c7e0068a5b5c185ff71
ganeshkumarjammu <shreeganeshkumar2k@gmail.com>
2022-12-20 05:26:18 +0530

    smartDoorLock_esp32 v2.1.2  successfull version

--- smartDoorLock_esp32/smartDoorLock_esp32.ino
+++ smartDoorLock_esp32/smartDoorLock_esp32.ino
@@ -17,7 +17,7 @@ String CHAT_ID = "-1001686432193";
 // "1260116757";
 
 bool sendPhoto = false;
-
+bool msgEnabled = true ;
 WiFiClientSecure clientTCP;
 UniversalTelegramBot bot(BOTtoken, clientTCP);
 
@@ -35,31 +35,34 @@ unsigned long lastTimeBotRan;
 #define LOCK3 15
 #define FLASH_LED 2
 
+#define TRIG_PIN 23 // ESP32 pin GIOP23 connected to Ultrasonic Sensor's TRIG pin
+#define ECHO_PIN 22 // ESP32 pin GIOP22 connected to Ultrasonic Sensor's ECHO pin
+float duration_us, distance_cm;
 
 int lockState = 0;
 String r_msg = "";
 
 String unlockDoor(){  
- if (lockState == 0) {
-  digitalWrite(LOCK, HIGH);
-  lockState = 1;
+  if (lockState == 1) {
+  digitalWrite(LOCK, LOW);
+  lockState = 0;
   delay(100);
   return "Door Unlocked. /lock";
  }
  else{
   return "Door Already Unlocked. /lock";
- }  
+ }
 }
 String lockDoor(){
- if (lockState == 1) {
-  digitalWrite(LOCK, LOW);
-  lockState = 0;
+ if (lockState == 0) {
+  digitalWrite(LOCK, HIGH);
+  lockState = 1;
   delay(100);
   return "Door Locked. /unlock";
  }
  else{
-  return "Door Already Locked. /unlock";
- }
+  return "Door Already locked. /unlock";
+ }  
 }
 
 
@@ -80,36 +83,47 @@ void handleNewMessages(int numNewMessages){
     // Print the received message
     String text = bot.messages[i].text;
     Serial.println(text);
-
     String fromName = bot.messages[i].from_name;
-    if (text == "/photo"  ) {
+    if ((text == "/photo") || (text == "/photo@Smart_Dora_bot" )) {
       sendPhoto = true;
       Serial.println("New photo request");
     }
 
-    if (text == "/lock"){
+    if ((text == "/lock") || (text == "/lock@Smart_Dora_bot" )){
       String r_msg = lockDoor();
       bot.sendMessage(CHAT_ID, r_msg, "");
     }
-    if (text == "/unlock"){
+    if ((text == "/unlock") || (text == "/unlock@Smart_Dora_bot" )){
       String r_msg = unlockDoor();
       bot.sendMessage(CHAT_ID, r_msg, "");
     }
-    if (text == "/start"){
+    if ((text == "/start") || (text == "/start@Smart_Dora_bot" )){
       String welcome = "Welcome to the ESP32 Telegram Smart Lock.\n";
-      welcome += "/photo : Takes a new photo\n";
-      welcome += "/flash : To toggle Flashlight\n";
-      welcome += "/unlock : Unlock the Door\n\n";
       welcome += "/lock : Lock the Door\n";
+      welcome += "/unlock : Unlock the Door\n\n";
+      welcome += "/lockStatus : Takes a know Lock Status\n";
+      welcome += "/notify : To turn on/off notificaton\n";
+      welcome += "/Buzz : To turn on buzzer\n";
       welcome += "To get the photo please tap on /photo.\n";
       bot.sendMessage(CHAT_ID, welcome, "Markdown");
     }
-    if (text == "/flash") {
+    if ((text == "/flash") || (text == "/flash@Smart_Dora_bot" )) {
       flashState = !flashState;
       String r_msg = "Flash toggled";
       bot.sendMessage(CHAT_ID, r_msg, "");
       digitalWrite(FLASH_LED_PIN, flashState);
       Serial.println("Change flash LED state");
+    }
+    if ((text == "/notify") || (text == "/notify@Smart_Dora_bot" )){
+      msgEnabled = !msgEnabled ;
+      if(msgEnabled){
+      String r_msg = "notification turned on";
+      bot.sendMessage(CHAT_ID, r_msg, "");
+      }
+      else{
+      String r_msg = "notification turned off";
+      bot.sendMessage(CHAT_ID, r_msg, ""); 
+      }
     }
   }
 }
@@ -121,10 +135,14 @@ void setup(){
   // Init Serial Monitor
   Serial.begin(115200);
 
+  // configure the trigger pin to output mode
+  pinMode(TRIG_PIN, OUTPUT);
+  // configure the echo pin to input mode
+  pinMode(ECHO_PIN, INPUT);
+  
   // Set LED Flash as output
   pinMode(FLASH_LED_PIN, OUTPUT); 
   digitalWrite(FLASH_LED_PIN, flashState);
-
 
   // Connect to Wi-Fi
   WiFi.mode(WIFI_STA);
@@ -137,7 +155,8 @@ void setup(){
     Serial.print(".");
     delay(500);
   }
-  Serial.println();
+  lockDoor();
+  Serial.println("Succesfully Connected ");
   Serial.print("ESP32 IP Address: ");
   Serial.println(WiFi.localIP()); 
 }
@@ -145,15 +164,6 @@ void setup(){
 void loop() {
   if (sendPhoto) {
     Serial.println("Preparing photo");
-    digitalWrite(FLASH_LED, HIGH);
-    delay(200);
-    digitalWrite(FLASH_LED, LOW);
-    sendPhoto = false; 
-  }
-  if(digitalRead(BUTTON) == HIGH){
-    Serial.println("Preparing photo");
-    String r_msg = "Button pressed";
-    bot.sendMessage(CHAT_ID, r_msg, "");
     digitalWrite(FLASH_LED, HIGH);
     delay(200);
     digitalWrite(FLASH_LED, LOW);
@@ -168,5 +178,26 @@ void loop() {
       numNewMessages = bot.getUpdates(bot.last_message_received + 1);
     }
     lastTimeBotRan = millis();
+  }
+  
+  // generate 10-microsecond pulse to TRIG pin
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
+  delayMicroseconds(2);
+  // measure duration of pulse from ECHO pin
+  duration_us = pulseIn(ECHO_PIN, HIGH);
+  delayMicroseconds(2);
+  // calculate the distance
+  distance_cm = 0.017 * duration_us;
+  
+  // print the value to Serial Monitor
+  Serial.print("distance: ");
+  Serial.print(distance_cm);
+  Serial.println(" cm");
+  delay(50);
+  if (distance_cm < 30 && msgEnabled ){
+      String r_msg = "Object detected , /photo"; //whether this text will be received by the esp32cam ??
+      bot.sendMessage(CHAT_ID, r_msg, "");
   }
 }
